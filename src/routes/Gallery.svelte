fcf96840a70fbe83bbcc61e7e166223910c2b56e
Maksymilian Dendura <maksd0410@gmail.com>
2022-11-22 20:06:10 +0100

    Updated gallery content

--- src/routes/Gallery.svelte
+++ src/routes/Gallery.svelte
@@ -5,17 +5,20 @@
 
 <ContentContainer>
   <Heading level="1">Galeria</Heading>
+  <p class="my-8 text-center text-lg font-semibold lg:text-2xl">
+    Poznaj nasz salon!
+  </p>
   <div
     class="flex flex-col justify-center items-center gap-4 mx-auto md:flex-row md:flex-wrap lg:max-w-4xl"
   >
-    <img src="img/photo-1.webp" alt="a" class="md:w-64" />
-    <img src="img/photo-2.jpeg" alt="a" class="md:w-64" />
-    <img src="img/photo-3.jpeg" alt="a" class="md:w-64" />
-    <img src="img/photo-2.jpeg" alt="a" class="md:w-64" />
-    <img src="img/photo-3.jpeg" alt="a" class="md:w-64" />
-    <img src="img/photo-1.webp" alt="a" class="md:w-64" />
-    <img src="img/photo-3.jpeg" alt="a" class="md:w-64" />
-    <img src="img/photo-1.webp" alt="a" class="md:w-64" />
-    <img src="img/photo-2.jpeg" alt="a" class="md:w-64" />
+    <img src="img/photo-1.webp" alt="Zdjęcie salonu" class="md:w-64" />
+    <img src="img/photo-2.webp" alt="Zdjęcie salonu" class="md:w-64" />
+    <img src="img/photo-3.webp" alt="Zdjęcie salonu" class="md:w-64" />
+    <img src="img/photo-2.webp" alt="Zdjęcie salonu" class="md:w-64" />
+    <img src="img/photo-3.webp" alt="Zdjęcie salonu" class="md:w-64" />
+    <img src="img/photo-1.webp" alt="Zdjęcie salonu" class="md:w-64" />
+    <img src="img/photo-3.webp" alt="Zdjęcie salonu" class="md:w-64" />
+    <img src="img/photo-1.webp" alt="Zdjęcie salonu" class="md:w-64" />
+    <img src="img/photo-2.webp" alt="Zdjęcie salonu" class="md:w-64" />
   </div></ContentContainer
 >
