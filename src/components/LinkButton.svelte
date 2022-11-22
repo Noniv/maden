0625afef0b58de57a637d13e5eb11985598dddd7
Maksymilian Dendura <maksd0410@gmail.com>
2022-11-22 20:04:42 +0100

    Added LinkButton component

---
+++ src/components/LinkButton.svelte
@@ -0,0 +1,13 @@
+<script>
+  export let className = "";
+  export let href = "";
+  export let content = "";
+</script>
+
+<a
+  {href}
+  class={"text-white bg-yellow-700 py-3 px-6 block w-fit rounded-full text-2xl hover:bg-yellow-800 " +
+    className}
+>
+  {content}
+</a>
