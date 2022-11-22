fd5b40d8b477f02be0ce931a9d0315cea3f390ab
Maksymilian Dendura <maksd0410@gmail.com>
2022-11-22 20:06:01 +0100

    Updated certificates content

--- src/routes/Certificates.svelte
+++ src/routes/Certificates.svelte
@@ -5,4 +5,11 @@
 
 <ContentContainer>
   <Heading level="1">Certyfikaty</Heading>
+  <p class="my-8 text-center text-lg font-semibold lg:text-2xl">
+    Nasi pracownicy ciągle poprawiają swoje kwalifikacje za pomocą zewnętrznych
+    szkoleń.
+  </p>
+  <img src="img/Certificate-1.webp" alt="Certyfikat" class="mb-16">
+  <img src="img/Certificate-2.webp" alt="Certyfikat" class="mb-16">
+  <img src="img/Certificate-3.webp" alt="Certyfikat" class="mb-16">
 </ContentContainer>
