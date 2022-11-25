b9a1243e248686a59040bb182b1aebd5baac6304
Maksymilian Dendura <maksd0410@gmail.com>
2022-11-25 10:39:56 +0100

    Added hover animation to team members

--- src/components/TeamMember.svelte
+++ src/components/TeamMember.svelte
@@ -4,7 +4,7 @@
     export let id = 0
 </script>
 
-<a href={"/#/zespol/" + id} class="border-yellow-700 border-b-[1px] block">
+<a href={"/#/zespol/" + id} class="border-yellow-700 border-b-[1px] block hover:shadow-lg relative hover:scale-[101%]">
     <img src={src} alt="Fryzjer" class="md:w-64">
     <div class="text-center py-4 text-lg font-semibold">
         {name}
